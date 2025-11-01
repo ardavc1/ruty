073a000da0c2b0611388dc204eed89d0a7c086f2
Arda Avcı <wonderluxct@gmail.com>
2025-11-01 15:04:28 +0300

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Merhaba Dünya!'),
         ),
       ),
     );
