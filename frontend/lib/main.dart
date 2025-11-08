671e422c824434aa881921f4877d1b7fe6b33971
Arda Avcı <wonderluxct@gmail.com>
2025-11-08 20:32:02 +0300

    main ayarlaması & fix

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MainApp());
-}
+import 'register_screen.dart';
+import 'habit_list.dart';
+import 'pet_selection_screen.dart';
+import 'habit_add_page.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Merhaba Dünya!'),
+    const primary = Color(0xFF499BCF); // #499bcf
+    return MaterialApp(
+      title: 'Ruty',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: primary),
+        useMaterial3: true,
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(),
         ),
       ),
+      initialRoute: '/auth',
+      routes: {
+        '/auth': (_) => const RegisterScreen(),
+        '/animal': (_) => const PetSelectionScreen(),
+        '/habit-add': (_) => const HabitAddPage(),
+        '/habit-list': (_) => const HabitListScreen(),
+      },
     );
   }
 }
